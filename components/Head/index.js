a394a2c0c34fea908ccaf919938578d2618c1391
manjaro <whj199910@gmail.com>
2021-07-19 23:14:26 +0800

    update: add metas

--- components/Head/index.js
+++ components/Head/index.js
@@ -2,14 +2,25 @@ import Head from 'next/head'
 
 const PageHead = ({ PageName }) => {
   return (
-        <Head>
-            <link rel="shortcut icon" href="logo.png"></link>
-            <meta name="viewport" content="width=device-width,
-                initial-scale=1.0,
-                maximum-scale=1.0,
-                user-scalable=0" />
-            <title>{PageName}</title>
-        </Head>
+    <Head>
+      <meta charset="UTF-8" />
+      <meta httpEquiv="x-dns-prefetch-control " content="on" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=Edge, chrome=1" />
+      <link rel="shortcut icon" href="logo.png" />
+      <meta name="viewport" content="width=device-width,
+        initial-scale=1.0,
+        maximum-scale=1.0,
+        user-scalable=0" />
+      <meta name="description" content="Kevin&apos;s diary" />
+      <meta property="og:title" content="Kevin&apos;s diary" />
+      <meta property="og:type" content="Website" />
+      <meta property="og:url" content="https://diary.harmless.blue/" />
+      <meta property="og:image" content="logo.png" />
+      <meta property="og:description" content="Kevin&apos;s diary" />
+      <meta name="twitter:card" content="summary" />
+      <link rel="canonical" href="https://diary.harmless.blue/" />
+      <title>{PageName}</title>
+    </Head>
   )
 }
 
