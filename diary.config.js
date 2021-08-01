66a725345769e756023b2db52f1cc9b55431f01f
kevin <44513103+crumbledwall@users.noreply.github.com>
2021-08-01 12:43:43 +0800

    Update: change api cdn

--- diary.config.js
+++ diary.config.js
@@ -1,7 +1,7 @@
 const DIARY = {
-  baseUrl: 'https://api.kev1n.top',
+  baseUrl: 'https://api-cdn.kev1n.top',
   username: 'crumbledwall',
-  repo: 'next-diary',
+  repo: 'diary-issue',
   link: 'https://diary.harmless.blue/',
   siteName: 'Kevin\'s Diary',
   perPage: 5
